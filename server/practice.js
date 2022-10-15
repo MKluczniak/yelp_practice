8ec256e52c2b5532977cde4aa2d5015003e63ab1
Mikolaj Kluczniak <mikolajkluczniak@MacBook-Pro-Mikolaj.local>
2022-10-15 14:25:45 +0200

    update delete routes done

---
+++ server/practice.js
@@ -0,0 +1,15 @@
+// app.use(morgan("tiny"))     //USING MORGAN   |  no need to use next()(already configured to do it)
+// //creating middleware
+// app.use((req,res, next) => {     //thats all it is, middleware is that simple all we need to do is say next() in or function so it will go one step furter to next middleware or to a final middleware-route
+//     console.log("yeah im a middleware")
+//     next(); //tells the middle were go to the next middlware  
+// });  //were you define a middleware is very important the program will go to the first middleware which maches that url(in our case it macha all urls any req can hit it) if we put it below our route it will never run
+
+// app.use((req, res, next) => {    //so from the middleware we can send a res to the user! ;) that how powerful midlleware is
+//     res.status(404).json({
+//         status: "fail"
+//     });
+// });
+
+// app.use((req,res, next) => {});  //that way (coz we dont call next(), we can DROP the request) anything u can do in route u can do also in middleware
+// //usualu you dont need to create your middleware there is ton of them in the internet you can do it with third party middleware"morgan"
--- server/server.js
+++ server/server.js
@@ -1,35 +1,70 @@
 // it is going to be our entry point for our backend application so its important file :p creatin and initialazing our express app
 //and actualy starting up and listening on some ports we define
-require('dotenv').config()  //we are going to require dotenv
-console.log(process.env)
+require('dotenv').config();  //we are going to require dotenv
+console.log(process.env);
 
 
-const express = require("express") //importing express app
+const express = require("express"); //importing express app
+const morgan = require('morgan'); //importing morgan
+const app = express();//creating an instance of our express app // so we created and instance of express an we stored it in a var app
 
-
-const app = express()//creating an instance of our express app // so we created and instance of express an we stored it in a var app
+// app.use(morgan("dev"))
+app.use(express.json())  //this middleware is going to take whats in the body (retrive the date in the body of request) and we do it with middleware express.json() and it is going to attach it to "req"
 
 //now we need to start building our API plus routs
-
 //get all restaurants
 app.get("/api/v1/restaurants", (req, res) => {
+    console.log("route handler run")
       res.status(200).json({      //we can use res.status(404).json to set our own status code and then see that in postman
         status: "succes",
         data: {
             restaurant: ["mcdonalds", "wendys"],
         }
-        
       }) //we can also sned a json object res.json({}) the response will come as a json format and once we build our react the react cli will recieve a json format so he can easli parse the data  //res.send("these are the restaurants") this will send a "res"ponse and write it on the website   //consol log would print in our consol where the server is running
 })  // the url parameter is the url the frontend is going to send a http request to, the second parametr (req, res) is the callback function with request & response(we send back) stored
 
 //getting an individual restaurant 
 
 app.get("/api/v1/restaurants/:id", (req, res) => {
-
-    console.log(req.params) //console.log(req) i wanna consol log the request object, it will print a big json object in consol where we can find for example "parems: id" so exacly what is our param in our route
-    
-    
+    console.log(req.params) //console.log(req) i wanna consol log the request object, it will print a big json object in consol where we can find for example "parems: id" so exacly what is our param in our route or better we send   console.log(req.params) we got { id: '1234' } so the id we provided in the url, so now we know which restaurant our user want to retive
+    res.status(200).json({
+        status: "succes",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
 })
+//crete restaureant
+app.post("/api/v1/restaurants", (req, res)=> {
+    console.log(req.body);
+    res.status(201).json({
+        status: "succes",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
+})
+
+//update restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.status(200).json({
+        status: "succes",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    })
+} )
+
+//delete restaurant
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
+    })
+})
+
 
 
 
